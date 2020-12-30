d64fb3e076ba528906b6bc862458f6944b13cc04
belbanas <belbanas@gmail.com>
2020-12-30 15:08:20 +0100

    Add base upload with renaming

---
+++ app/models/Upload.php
@@ -0,0 +1,22 @@
+<?php
+
+
+namespace app\models;
+
+
+class Upload
+{
+    private string $uploadFolder = "etlapok/";
+
+    public function uploadFile($files, $post)
+    {
+        $week = substr($post['week'], -2);
+        echo $week;
+        $target = $this->uploadFolder.$week."_Het_HU.xls";
+        if (file_exists($target)) {
+            echo '<script type="text/javascript">alert("Az adott hétre már van étlap, kérlek válassz másikat")</script>';
+        } else {
+            move_uploaded_file($files["etlap"]["tmp_name"], $target);
+        }
+    }
+}
--- index.php
+++ index.php
@@ -1,9 +1,22 @@
 <?php
 
 use app\controllers\Route;
+use app\models\Upload;
 use app\models\Pult;
 
 require_once __DIR__ . '/vendor/autoload.php';
+
+Route::add('/upload', function () {
+    include('upload.html');
+}, 'get');
+
+Route::add('/upload', function () {
+    $upload = new Upload();
+    $upload->uploadFile($_FILES, $_POST);
+    var_dump($_POST);
+    var_dump($_FILES);
+    Header('Location: /upload');
+}, 'post');
 
 Route::add('/json-output', function () {
     $pultId = $_GET['id'];
