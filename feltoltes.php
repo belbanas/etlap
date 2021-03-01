a7bf33ab7eaa8194f34dc7a017e5407784a0d64b
belbanas <belbanas@gmail.com>
2021-03-01 12:06:38 +0100

    refactor feltoltes.php

--- feltoltes.php
+++ feltoltes.php
@@ -6,8 +6,8 @@ require_once __DIR__ . '/vendor/autoload.php';
 
 $upload = new Upload();
 $upload->uploadFile($_FILES, $_POST);
-var_dump($_POST);
-var_dump($_FILES);
+//var_dump($_POST);
+//var_dump($_FILES);
 
 ?>
 
