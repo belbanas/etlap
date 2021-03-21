9043e73f4af2668fb19b351dfe8e439207b371d6
belbanas <belbanas@gmail.com>
2021-03-21 10:59:24 +0100

    remove css styling from feltoltes

--- feltoltes.php
+++ feltoltes.php
@@ -18,7 +18,7 @@ $upload->uploadFile($_FILES, $_POST);
     <meta name="viewport"
           content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
-    <link rel="stylesheet" href="static/css/style.css">
+    <!-- <link rel="stylesheet" href="static/css/style.css"> -->
     <title>Étlap feltöltése</title>
 </head>
 <body>
